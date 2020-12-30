736c71687539025f19554cc408d96414ad72a9d1
User <User@DESKTOP-6DMK7N8>
2020-12-30 14:50:37 +0300

    Добавьте файлы проекта.

---
+++ Task18/MathStatistics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task18
+{
+    static class MathStatistics
+    {
+        public static double RoundTo(double number, int digits)
+        {
+            double factor = Math.Pow(10, digits);
+            return Math.Round(number * factor) / factor;
+        }
+
+        public static double CalcSum(params double[] nums)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+                sum += nums[i];
+
+            return sum;
+        }
+
+        public static double CalcAverage(params double[] nums) => CalcSum(nums) / nums.Length;
+
+        public static double CalcDispercion(params double[] nums)
+        {
+            double avg = CalcAverage(nums);
+            double sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+                sum += nums[i] * nums[i];
+
+            return sum / nums.Length - avg * avg;
+        }
+
+        public static double CalcStandardDeviation(params double[] nums)
+        {
+            return Math.Sqrt(CalcDispercion(nums));
+        }
+
+        public static double CalcCoefficientOfVariation(params double[] nums) => CalcStandardDeviation(nums) / CalcAverage(nums) * 100;
+    }
+}
+
---
+++ Task18/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task18
+{
+    class Program
+    {
+        static void EndOfCase()
+        {
+            Console.WriteLine("press any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main(string[] args)
+        {
+            string input;
+            bool checkInput;
+            double value;
+            double result;
+            List<double> nums = new List<double>();
+
+            Console.WriteLine("enter number or 'n' to complete the input: ");
+
+            while (true)
+            {
+                input = Console.ReadLine();
+                checkInput = double.TryParse(input, out value);
+
+                if (!checkInput && input != "n")
+                {
+                    Console.WriteLine("wrong input format!");
+                    break;
+                }
+                else if (!checkInput)
+                {
+                    break;
+                }
+                else
+                {
+                    nums.Add(value);
+                }
+            }
+
+            if (nums.Count < 2)
+            {
+                Console.WriteLine("\noperations can't be perfomed, too few arguments");
+            }
+            else
+            {
+                double[] numsArr = nums.ToArray();
+                Console.WriteLine("\n");
+
+                start:
+
+                Console.Clear();
+
+                Console.WriteLine("entered numbers");
+                foreach (var n in nums)
+                    Console.Write(n + " ");
+                Console.WriteLine("\n");
+
+                Console.WriteLine("1.Add");
+                Console.WriteLine("2.Calculate average");
+                Console.WriteLine("3.Calculate dispercion");
+                Console.WriteLine("4.Calculate standard deviation");
+                Console.WriteLine("5.Calculate coefficient of variation");
+                Console.WriteLine("\n6.exit\n");
+
+                int opNum = 0;
+                checkInput = false;
+
+                while (!checkInput)
+                {
+                    Console.WriteLine("enter number of operation: ");
+                    checkInput = int.TryParse(Console.ReadLine(), out opNum);
+                }
+
+                switch (opNum)
+                {
+                    case 1:
+                        Console.Clear();
+
+                        result = MathStatistics.CalcSum(numsArr);
+                        result = MathStatistics.RoundTo(result, 2);
+                        Console.WriteLine("sum = " + result);
+
+                        EndOfCase();
+
+                        goto start;
+                    case 2:
+                        Console.Clear();
+
+                        result = MathStatistics.CalcAverage(numsArr);
+                        result = MathStatistics.RoundTo(result, 2);
+                        Console.WriteLine("average = " + result);
+
+                        EndOfCase();
+
+                        goto start;
+                    case 3:
+                        Console.Clear();
+
+                        result = MathStatistics.CalcDispercion(numsArr);
+                        result = MathStatistics.RoundTo(result, 2);
+                        Console.WriteLine("dispercion = " + result);
+
+                        EndOfCase();
+
+                        goto start;
+                    case 4:
+                        Console.Clear();
+
+                        result = MathStatistics.CalcStandardDeviation(numsArr);
+                        result = MathStatistics.RoundTo(result, 2);
+                        Console.WriteLine("standard deviation = " + result);
+
+                        EndOfCase();
+
+                        goto start;
+                    case 5:
+                        Console.Clear();
+
+                        result = MathStatistics.CalcCoefficientOfVariation(numsArr);
+                        result = MathStatistics.RoundTo(result, 2);
+                        Console.WriteLine("coefficient of variation = " + result);
+
+                        EndOfCase();
+
+                        goto start;
+                    case 6:
+                        Console.Clear();
+
+                        EndOfCase();
+
+                        break;
+                    default:
+                        Console.WriteLine("wrong number!");
+
+                        EndOfCase();
+
+                        goto start;
+                }
+
+            }
+        }
+    }
+}
